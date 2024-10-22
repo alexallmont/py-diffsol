92ebac2303683888fcbd4c7a0307fde162552503
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2024-10-22 17:12:02 +0000

    style: disambiguate diffsol crate from main module (#5)
    - Renaming main method and giving explicit name means leading `::` is not necessary to disambiguate from crate.
    - This was found whilst experimenting renaming project to distinguish it from underlying diffsol package in generated docs. Unfortunately the lib name must match the shared object name, so overall rename needs further investigation.

--- src/lib.rs
+++ src/lib.rs
@@ -10,22 +10,22 @@ mod faer_py;
 
 // nalgebra_dense_lu_f64 binding
 type MatrixNaLu = nalgebra::DMatrix<f64>;
-type SolverNaLu<Op> = ::diffsol::NalgebraLU<f64, Op>;
+type SolverNaLu<Op> = diffsol::NalgebraLU<f64, Op>;
 create_binding!(nalgebra_dense_lu_f64, MatrixNaLu, SolverNaLu, nalgebra_py);
 
 // faer_sparse_lu_f64 binding
-type MatrixFaLu = ::diffsol::SparseColMat<f64>;
-type SolverFaLu<Op> = ::diffsol::FaerSparseLU<f64, Op>;
+type MatrixFaLu = diffsol::SparseColMat<f64>;
+type SolverFaLu<Op> = diffsol::FaerSparseLU<f64, Op>;
 create_binding!(faer_sparse_lu_f64, MatrixFaLu, SolverFaLu, faer_py);
 
 // faer_sparse_klu_f64 binding
-type MatrixFaKlu = ::diffsol::SparseColMat<f64>;
-type SolverFaKlu<Op> = ::diffsol::KLU<MatrixFaKlu, Op>;
+type MatrixFaKlu = diffsol::SparseColMat<f64>;
+type SolverFaKlu<Op> = diffsol::KLU<MatrixFaKlu, Op>;
 create_binding!(faer_sparse_klu_f64, MatrixFaKlu, SolverFaKlu, faer_py);
 
 /// Top-level typed diffsol bindings
-#[pymodule]
-fn diffsol(m: &Bound<'_, PyModule>) -> PyResult<()> {
+#[pymodule(name="diffsol")]
+fn py_diffsol(m: &Bound<'_, PyModule>) -> PyResult<()> {
     nalgebra_dense_lu_f64::add_to_parent_module(m)?;
     faer_sparse_lu_f64::add_to_parent_module(m)?;
     faer_sparse_klu_f64::add_to_parent_module(m)?;
